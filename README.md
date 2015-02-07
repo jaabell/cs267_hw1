e7006ae87a1d627294dfd9a4db509609f077834e
byeah <jby.yeah@gmail.com>
2015-02-06 22:25:34 -0800

    Right vectorization with -O3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # cs267_hw1
-Homework 1 for CS261 class at UCBerkeley: Optimizing Matrix Multiply
+Homework 1 for CS267 class at UCBerkeley: Optimizing Matrix Multiply
 
 By: Biye Jiang and Jose Abell
--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -25,22 +25,31 @@ inline static void do_block (int lda, int M, int N, int K, double* restrict A, d
 {
     for(int j=0;j<N;j+=REGBLOCK)
         for(int k=0;k<K;k+=REGBLOCK){
-            double *BB=B+j*lda+k;
+            double *restrict BB=B+j*lda+k;
             double b00=BB[0];
             double b10=BB[1];
             double b01=BB[lda];
             double b11=BB[1+lda];
             
-            double *AA=A+k*lda;
-            double *CC=C+j*lda;
+            double *restrict AA=A+k*lda;
+            double *restrict CC=C+j*lda;
+//            __builtin_assume_aligned(AA, 64);
+//            __builtin_assume_aligned(CC, 64);
             int i=0;
-            for(i=0;i+3<M;i+=4)//REGBLOCK)
+            //for(i=0;i+3<M;i+=4)//REGBLOCK)
+//            for(i=0;i<M;i++)
+
+            for(i=0;i<M;i++)
             {
+                CC[i]+=b00*AA[i]+b10*AA[i+lda];
+                CC[i+lda]+=b01*AA[i]+b11*AA[i+lda];
+                
+               
                 //int MM=min(M-i,REGBLOCK);
                 //int NN=min(N-j,REGBLOCK);
                 //int KK=min(K-k,REGBLOCK);
                 
-                double a0=AA[i];
+                /*double a0=AA[i];
                 double a1=AA[i+lda];
                 CC[i]+=a0*b00+a1*b10;
                 CC[i+lda]+=a0*b01+a1*b11;
@@ -55,7 +64,7 @@ inline static void do_block (int lda, int M, int N, int K, double* restrict A, d
                 a0=AA[i+3];
                 a1=AA[i+3+lda];
                 CC[i+3]+=a0*b00+a1*b10;
-                CC[i+3+lda]+=a0*b01+a1*b11;
+                CC[i+3+lda]+=a0*b01+a1*b11;*/
                 /*for(int jj=0;jj<NN;jj++)
                 {
                     for(int kk=0;kk<KK;kk++){
@@ -75,7 +84,7 @@ inline static void do_block (int lda, int M, int N, int K, double* restrict A, d
         }
 }
 
-void square_dgemm (int lda, double __restrict__  *A, double *  __restrict__  B,  double *restrict C)
+void square_dgemm (int lda, double* __restrict__  A, double *  __restrict__  B,  double * __restrict__ C)
 {
     // static double *restrict Abuf = buf1;
     static double *restrict Bbuf = buf2;
@@ -99,7 +108,7 @@ void square_dgemm (int lda, double __restrict__  *A, double *  __restrict__  B, 
             {
                 int M = min (BLOCK_SIZE, lda - i);
 
-                if (lda%2==0)
+                if (N%2==0&&K%2==0)
                     do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
                 else{
                 // for (int ii = 0; ii < M; ++ii)
@@ -109,14 +118,14 @@ void square_dgemm (int lda, double __restrict__  *A, double *  __restrict__  B, 
                 //     }
                 // do_block inlined
                 for (int jj = 0; jj < N; ++jj){
-                    double *CC=C+(i+(j+jj)*lda);
+                    double *restrict CC=C+(i+(j+jj)*lda);
                     for (int kk = 0; kk < K; ++kk)
                     {
                         // double bkkjj = bkj_block[kk + jj * lda];
-                        double *AA=A+(i+(k+kk)*lda);
+                        double *restrict AA=A+(i+(k+kk)*lda);
                         double bb=Bbuf[kk+K*jj];
                         int ii=0;
-                        for (ii = 0; ii+7 < M; ii+=8)
+                        for (ii = 0; ii < M; ii++)
                         {
                             // double   *restrict ciijj = C + (i + ii) + (j + jj) * lda ;
                             // double aiikk = A [i + ii + (k + kk) * lda ];
@@ -124,14 +133,14 @@ void square_dgemm (int lda, double __restrict__  *A, double *  __restrict__  B, 
                             // *ciijj += Abuf[ii + M * kk] * Bbuf[kk + K * jj];
                             // Cbuf[ii + M * jj] += Abuf[ii + M * kk] * Bbuf[kk + K * jj];
                             // Cbuf[ii + M * jj] += aiikk * Bbuf[kk + K * jj];
-                            CC[ii] +=AA[ii]*bb;
+                            CC[ii] +=AA[ii]*bb;/*
                             CC[ii+1]+=AA[ii+1]*bb;
                             CC[ii+2]+=AA[ii+2]*bb;
                             CC[ii+3]+=AA[ii+3]*bb;
                             CC[ii+4]+=AA[ii+4]*bb;
                             CC[ii+5]+=AA[ii+5]*bb;
                             CC[ii+6]+=AA[ii+6]*bb;
-                            CC[ii+7]+=AA[ii+7]*bb;
+                            CC[ii+7]+=AA[ii+7]*bb;*/
                             //C [(i + ii) + (j + jj) * lda ] += A [i + ii + (k + kk) * lda ] * Bbuf[kk + K * jj];
                         }
                         for(;ii<M;ii++)
